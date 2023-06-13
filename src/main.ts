6c497edcfb041f05475e9acb4114b0c25bf1de21
Aaron Hooper <rnjckhpr@gmail.com>
2023-06-13 16:44:33 +0100

    Port rust code to typescript

---
+++ src/main.ts
@@ -0,0 +1,130 @@
+import { List, Range, setIn } from "immutable";
+
+enum CellState {
+  Dead = "DEAD",
+  Alive = "ALIVE",
+}
+
+type Coords = [x: number, y: number];
+type GridState = List<List<CellState>>;
+
+function toggleCell(state: GridState, point: Coords): GridState {
+  const [x, y] = point;
+  const cell = at(state, point);
+  const toggle = (c: CellState) =>
+    c === CellState.Alive ? CellState.Dead : CellState.Alive;
+
+  return setIn(state, [y, x], toggle(cell));
+}
+
+function at(state: GridState, point: Coords): CellState {
+  const [x, y] = point;
+  const cell = state.get(y)?.get(x);
+
+  if (cell === undefined) {
+    throw new RangeError("Point must be within grid bounds");
+  }
+
+  return cell;
+}
+class Grid {
+  private _width: number;
+  private _height: number;
+  private _state: GridState;
+
+  constructor(width: number, height: number) {
+    if (width <= 0 || height <= 0) {
+      throw new RangeError("width and height must be positive and non-zero");
+    }
+
+    this._width = width;
+    this._height = height;
+    this._state = Range(0, height)
+      .map(() => List(Array(width).fill(CellState.Dead)))
+      .toList();
+  }
+
+  neighbors(point: Coords): Coords[] {
+    const [x, y] = point;
+    const points: Coords[] = [];
+
+    for (let yOffset = -1; yOffset <= 1; yOffset++) {
+      for (let xOffset = -1; xOffset <= 1; xOffset++) {
+        if (xOffset === 0 && yOffset === 0) {
+          continue; // The starting point cannot be a neighbor, so ignore
+        }
+
+        const neighbor: Coords = [x + xOffset, y + yOffset];
+
+        if (!this.inBounds(neighbor)) {
+          continue;
+        }
+
+        points.push(neighbor);
+      }
+    }
+
+    return points;
+  }
+
+  inBounds(point: Coords): boolean {
+    const [x, y] = point;
+    const xInBounds = x >= 0 && x < this._width;
+    const yInBounds = y >= 0 && y < this._height;
+
+    return xInBounds && yInBounds;
+  }
+
+  get state() {
+    return List(this._state);
+  }
+
+  set state(s) {
+    this._state = List(s);
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+}
+
+class Game {
+  grid: Grid;
+  generation: number;
+
+  constructor(grid: Grid) {
+    this.grid = grid;
+    this.generation = 0;
+  }
+
+  next() {
+    let nextGridState = this.grid.state;
+
+    for (let i = 0; i < this.grid.height; i++) {
+      for (let j = 0; j < this.grid.width; j++) {
+        const point: Coords = [j, i];
+        const cellState = at(this.grid.state, point);
+
+        const liveNeighborCount = this.grid
+          .neighbors(point)
+          .filter((p) => at(this.grid.state, p) === CellState.Alive).length;
+
+        if (
+          cellState === CellState.Alive &&
+          (liveNeighborCount < 2 || liveNeighborCount > 3)
+        ) {
+          nextGridState = toggleCell(nextGridState, point);
+        } else if (cellState === CellState.Dead && liveNeighborCount === 3) {
+          nextGridState = toggleCell(nextGridState, point);
+        }
+      }
+    }
+
+    this.grid.state = nextGridState;
+    this.generation += 1;
+  }
+}
