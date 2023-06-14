b1dcf23590d57931544ba16d0b2f44822b8c09a9
Aaron Hooper <rnjckhpr@gmail.com>
2023-06-14 10:22:30 +0100

    Write beacon unit test

--- src/test/game.test.ts
+++ src/test/game.test.ts
@@ -16,9 +16,34 @@ describe("game", () => {
     const game = new Game(gen0);
 
     game.next();
-    expect(game.grid).toEqual(gen1);
+    expect(game.grid.state).toEqual(gen1.state);
 
     game.next();
-    expect(game.grid).toEqual(gen0);
+    expect(game.grid.state).toEqual(gen0.state);
+  });
+
+  it("renders a beacon", () => {
+    const gen0 = new Grid(6, 6);
+    gen0.state = Grid.toggle(gen0.state, [1, 1]);
+    gen0.state = Grid.toggle(gen0.state, [1, 2]);
+    gen0.state = Grid.toggle(gen0.state, [2, 1]);
+    gen0.state = Grid.toggle(gen0.state, [2, 2]);
+    gen0.state = Grid.toggle(gen0.state, [3, 3]);
+    gen0.state = Grid.toggle(gen0.state, [3, 4]);
+    gen0.state = Grid.toggle(gen0.state, [4, 3]);
+    gen0.state = Grid.toggle(gen0.state, [4, 4]);
+
+    const gen1 = new Grid(6, 6);
+    gen1.state = gen0.state;
+    gen1.state = Grid.toggle(gen1.state, [2, 2]);
+    gen1.state = Grid.toggle(gen1.state, [3, 3]);
+
+    const game = new Game(gen0);
+
+    game.next();
+    expect(game.grid.state).toEqual(gen1.state);
+
+    game.next();
+    expect(game.grid.state).toEqual(gen0.state);
   });
 });
