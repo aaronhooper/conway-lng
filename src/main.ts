63c67eca8e2c45046be989ad572064f6731d28d8
Aaron Hooper <rnjckhpr@gmail.com>
2023-06-14 15:11:38 +0100

    Render with canvas

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import { Game } from "./lib/game";
+import { Grid } from "./lib/grid";
+import { CellState, Coords } from "./lib/types";
+
+const CELL_WIDTH = 20;
+const GAME_WIDTH = 50;
+const GAME_HEIGHT = 30;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const grid = new Grid(GAME_WIDTH, GAME_HEIGHT);
+const gridCoords: Coords[] = [
+  [1, 3],
+  [2, 1],
+  [2, 3],
+  [3, 2],
+  [3, 3],
+];
+
+for (const point of gridCoords) {
+  grid.state = Grid.toggle(grid.state, point);
+}
+
+const game = new Game(grid);
+
+function draw(): void {
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  game.next();
+
+  for (let i = 0; i < GAME_HEIGHT; i++) {
+    for (let j = 0; j < GAME_WIDTH; j++) {
+      const cell = Grid.at(game.grid.state, [j, i]);
+
+      if (cell === CellState.Alive) {
+        ctx.fillStyle = "black";
+      } else if (cell === CellState.Dead) {
+        ctx.fillStyle = "white";
+      }
+
+      ctx.fillRect(
+        j * (CELL_WIDTH + 1),
+        i * (CELL_WIDTH + 1),
+        CELL_WIDTH,
+        CELL_WIDTH
+      );
+    }
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+draw();
